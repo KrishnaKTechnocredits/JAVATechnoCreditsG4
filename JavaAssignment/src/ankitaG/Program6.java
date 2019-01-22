183c11ffe0dd66c1285683f4987be5f1f2b1ac9d
AnkitaGTechnocredits <gankitagupta08@gmail.com>
2019-01-22 22:51:11 +0530

    Assignment_Q6_ankita

--- JavaAssignment/src/ankitaG/Program6.java
+++ JavaAssignment/src/ankitaG/Program6.java
@@ -7,12 +7,10 @@ public class Program6 {
        float celciusTem= (temperatureValue-32f)*5/9;
        System.out.println("temperature in celcius is:" +celciusTem+"^C");
 	}
-	public static void main(String[] args) {
-		
-	float temp=Float.parseFloat(args[0]);
-	Program6 p6= new Program6();
-	p6.temperatureConvertor(temp);
-	
+	public static void main(String[] args)
+	{
+	   float temp=Float.parseFloat(args[0]);
+	   Program6 p6= new Program6();
+	   p6.temperatureConvertor(temp);	
 	}
-
 }

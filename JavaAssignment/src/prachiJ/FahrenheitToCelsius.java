44b9ee0676691dc74b616a965c37575ef4b8ca32
PrachiJTechnocredits <jainprachi.cs@gmail.com>
2019-01-26 23:54:20 +0530

    Addressed Comments

--- JavaAssignment/src/prachiJ/FahrenheitToCelsius.java
+++ JavaAssignment/src/prachiJ/FahrenheitToCelsius.java
@@ -3,16 +3,19 @@ package prachiJ;
 import prachiJ.FahrenheitToCelsius;
 
 public class FahrenheitToCelsius {
-
-    static void convertFToC(int f)
-	{
-	float c = (f-32)*(float)5/9;
-	System.out.println("Temprature " + f + "F" + " to celcius is: " +c +"C");	
+	/*
+	 * This method is to convert temperature from Fahrenheit To Celsius
+	 * @param f This is to pass Fahrenheit value to method 
+	 * @return Nothing
+	 */
+	static void convertFahrenheitToCelsius(int fahrenheit) {
+		float celcius = (fahrenheit - 32) * (float) 5 / 9;
+		System.out.println("Temprature " + fahrenheit + "F" + " to celcius is: " + celcius + "C");
 	}
+
 	public static void main(String[] args) {
-		
-		int f = Integer.parseInt(args[0]);
-		convertFToC(f);
-	}
 
+		int f = Integer.parseInt(args[0]);
+		convertFahrenheitToCelsius(f);
+	}
 }

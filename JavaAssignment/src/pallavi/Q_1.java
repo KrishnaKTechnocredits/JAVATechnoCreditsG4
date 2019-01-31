eeb230c2ab8d40dcb5b837ece43cc6e92350f671
PiyushSTechnocredits <piyushec1210@gmail.com>
2019-01-31 20:30:24 +0530

    Adding new file

---
+++ JavaAssignment/src/pallavi/Q_1.java
@@ -0,0 +1,35 @@
+package pallavi;
+
+public class Q_1 {
+
+	static void operations(int num1, int num2) {
+
+		int addition = num1 + num2;
+
+		System.out.println("Addition of two numbers is " + addition);
+
+		int subtraction = num1 - num2;
+
+		System.out.println("Subtraction of two numbers is " + subtraction);
+
+		int multiplication = num1 * num2;
+
+		System.out.println("Multiplication of two numbers is " + multiplication);
+
+		int division = num1 / num2;
+
+		System.out.println("Division of two numbers is " + division);
+
+		int remainder = num1 % num2;
+
+		System.out.println("Remainer of two numbers is " + remainder);
+
+	}
+
+	public static void main(String[] args) {
+
+		operations(25, 4);
+
+	}
+
+}
